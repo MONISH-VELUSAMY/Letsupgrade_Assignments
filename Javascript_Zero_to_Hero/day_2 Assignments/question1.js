aad5f19b5f792a10fca2ce7f6aa838d1899ea582
MONISH V S <52309838+MONISH-VELUSAMY@users.noreply.github.com>
2021-08-04 20:08:07 +0530

    day_2 assignments submitted

---
+++ Javascript_Zero_to_Hero/day_2 Assignments/question1.js
@@ -0,0 +1,10 @@
+const array=["Audi","BMW","Ford","Jaguar","Rolls_Royce"];
+console.log("given array:");
+console.log("array=[Audi,BMW,Ford,Jaguar,Rolls_Royce]");
+console.log("The reversed array:");
+for(let i=array.length;i>=0;i--){
+    
+   console.log(array[i]);
+
+
+}
---
+++ Javascript_Zero_to_Hero/day_2 Assignments/question2.js
@@ -0,0 +1,7 @@
+
+console.log("items that are divisible by 5 from 1 to 50:");
+for(let i=1;i<=50;i++){
+    if(i%5 == 0){
+        console.log(i);
+    }
+}
